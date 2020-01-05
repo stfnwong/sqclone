51e54b4b05d15140debab2bf271f1e0c3eb4dc10
Stefan Wong <stfnwong@gmail.com>
2020-01-05 17:38:23 +0000

    Changed constants to macros

--- src/table.c
+++ src/table.c
@@ -15,25 +15,6 @@
 #include <string.h>
 #include "table.h"
 
-// Constants for Table structure
-const uint32_t ID_SIZE = size_of_attribute(Row, id);
-const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
-
-/* 
- * The above means that we have the following layout in memory
- *
- *  column      size (bytes)     offset 
- *  id          4                0
- *  username    32               4
- *  email       255              36
- *  total       291
- */
-const uint32_t PAGE_SIZE      = 4096;        // same as OS VM page size 
 
 // ================ ROW
 
@@ -89,38 +70,6 @@ typedef enum
     NODE_LEAF
 } NodeType;
 
-
-/*
- * Common Node Header Layout
- */
-const uint32_t NODE_TYPE_SIZE          = sizeof(uint8_t);
-const uint32_t NODE_TYPE_OFFSET        = 0;
-const uint32_t IS_ROOT_SIZE            = sizeof(uint8_t);
-const uint32_t IS_ROOT_OFFSET          = NODE_TYPE_SIZE;
-const uint32_t PARENT_POINTER_SIZE     = sizeof(uint32_t);
-const uint32_t PARENT_POINTER_OFFSET   = IS_ROOT_OFFSET + IS_ROOT_SIZE;
-const uint32_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE +\
-                IS_ROOT_SIZE + PARENT_POINTER_SIZE;
-
-
-/*
- * Leaf Node Header Layout
- */
-const uint32_t LEAF_NODE_NUM_CELLS_SIZE   = sizeof(uint32_t);
-const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
-const uint32_t LEAF_NODE_HEADER_SIZE      = \
-                COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
-
-/*
- * Leaf Node Body Layout
- */
-const uint32_t LEAF_NODE_KEY_SIZE     = sizeof(uint32_t);
-const uint32_t LEAF_NODE_KEY_OFFSET   = 0;
-const uint32_t LEAF_NODE_VALUE_SIZE   = ROW_SIZE;     
-const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
-const uint32_t LEAF_NODE_CELL_SIZE    = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
-const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
-const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
 
 /*
  * Leaf Node methods
@@ -452,7 +401,7 @@ void leaf_node_insert(Cursor* cursor, uint32_t key, Row* value)
         {
             memcpy(
                leaf_node_cell(node, i),
-               leaf_mode_cell(node, i-1),
+               leaf_node_cell(node, i-1),
                LEAF_NODE_CELL_SIZE
            );
         }
--- src/table.h
+++ src/table.h
@@ -14,6 +14,29 @@
 
 #include <stdint.h>
 #include <string.h>
+
+// Attribute size
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
+
+// Constants for Table structure
+#define ID_SIZE          size_of_attribute(Row, id)
+#define USERNAME_SIZE    size_of_attribute(Row, username)
+#define EMAIL_SIZE       size_of_attribute(Row, email)
+#define ID_OFFSET        0
+#define USERNAME_OFFSET  (ID_OFFSET + ID_SIZE)
+#define EMAIL_OFFSET     (USERNAME_OFFSET + USERNAME_SIZE)
+#define ROW_SIZE         (ID_SIZE + USERNAME_SIZE + EMAIL_SIZE)
+
+/* 
+ * The above means that we have the following layout in memory
+ *
+ *  column      size (bytes)     offset 
+ *  id          4                0
+ *  username    32               4
+ *  email       255              36
+ *  total       291
+ */
+#define PAGE_SIZE 4096        // same as OS VM page size 
 /*
  * print_page_info()
  */
@@ -51,8 +74,6 @@ void print_row(Row* row);
  */
 
 // Compact representation of a Row 
-#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
-
 void serialize_row(Row* src, void* dest);
 void deserialize_row(void* src, Row* dst);
 
@@ -104,6 +125,35 @@ Cursor* table_start(Table* table);
 Cursor* table_end(Table* table);
 void*   cursor_value(Cursor* cursor);
 void    cursor_advance(Cursor* cursor);
+
+/*
+ * Common Node Header Layout
+ */
+#define NODE_TYPE_SIZE           sizeof(uint8_t)
+#define NODE_TYPE_OFFSET         0
+#define IS_ROOT_SIZE             sizeof(uint8_t)
+#define IS_ROOT_OFFSET           NODE_TYPE_SIZE
+#define PARENT_POINTER_SIZE      sizeof(uint32_t)
+#define PARENT_POINTER_OFFSET    (IS_ROOT_OFFSET + IS_ROOT_SIZE)
+#define COMMON_NODE_HEADER_SIZE  (NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE)
+
+/*
+ * Leaf Node Header Layout
+ */
+#define LEAF_NODE_NUM_CELLS_SIZE   sizeof(uint32_t)
+#define LEAF_NODE_NUM_CELLS_OFFSET COMMON_NODE_HEADER_SIZE
+#define LEAF_NODE_HEADER_SIZE      (COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE)
+/*
+ * Leaf Node Body Layout
+ */
+#define LEAF_NODE_KEY_SIZE        sizeof(uint32_t)
+#define LEAF_NODE_KEY_OFFSET      0
+#define LEAF_NODE_VALUE_SIZE      ROW_SIZE     
+#define LEAF_NODE_VALUE_OFFSET    (LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE)
+#define LEAF_NODE_CELL_SIZE       (LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE)
+#define LEAF_NODE_SPACE_FOR_CELLS (PAGE_SIZE - LEAF_NODE_HEADER_SIZE)
+#define LEAF_NODE_MAX_CELLS       (LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE)
+
 
 /*
  * Tree Nodes
